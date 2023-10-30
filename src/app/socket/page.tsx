7169ae3862f56254d83a9416a14aa02a7af22ca8
Noah Clark <noahammon00@gmail.com>
2023-10-30 12:28:39 -0600

    testing queue updvote and downvote functionality

--- src/app/socket/page.tsx
+++ src/app/socket/page.tsx
@@ -1,16 +1,17 @@
 'use client'
 import { useState } from "react"
 import { io } from "socket.io-client"
+import { Track } from "@spotify/web-api-ts-sdk"
+import SongDisplay from "./songDisplay"
 
 const socket = io('http://localhost:5678')
 
-interface song {
-    name: string,
-    votes: number
+interface QueueTrack extends Partial<Track> {
+  votes: number
 }
 
 export default function page() {
-    const [queue, setQueue] = useState<song[]>([
+    const [queue, setQueue] = useState<QueueTrack[]>([
         { name: 'Even if she falls', votes: 0},
         { name: 'Old pine', votes: 0},
         { name: 'Landmines', votes: 0},
@@ -18,6 +19,10 @@ export default function page() {
         { name: 'Tremors', votes: 0}
     ])
   return (
-    <div>page</div>
+    <div>
+      {
+        queue.sort((a, b) => b.votes - a.votes).map((song, index) => <SongDisplay key={index} song={song}/>)
+      }
+    </div>
   )
 }
--- src/app/socket/songDisplay.tsx
+++ src/app/socket/songDisplay.tsx
@@ -1,8 +1,68 @@
-'use client'
-import { useState } from "react"
+"use client";
+import { useState, useEffect } from "react";
+import { Track } from "@spotify/web-api-ts-sdk";
 
-export default function songDisplay() {
+interface QueueTrack extends Partial<Track> {
+  votes: number;
+}
+
+interface DispSongProps {
+  song: QueueTrack;
+}
+
+export default function SongDisplay(props: DispSongProps) {
+  const [userVote, setUserVote] = useState("neutral");
+
+  const handleClick = (vote: string) => {
+    switch (userVote) {
+      case 'neutral':
+        setUserVote(vote)
+        if (vote === 'upvoted') props.song.votes++
+        if (vote === 'downvoted') props.song.votes--
+        break
+      case 'upvoted':
+        if (vote === 'upvoted') {
+          props.song.votes--
+          setUserVote('neutral')
+        } else {
+          props.song.votes -= 2
+          setUserVote(vote)
+        }
+        break
+      case 'downvoted':
+        if (vote === 'downvoted') {
+          props.song.votes++
+          setUserVote('neutral')
+        } else {
+          props.song.votes += 2
+          setUserVote(vote)
+        }
+        break
+    }
+  };
+
   return (
-    <div>songDisplay</div>
-  )
+    <div className="flex justify-between">
+      <p>{props.song.name}</p>
+      <div>
+        <button
+          className={
+            userVote === "upvoted" ? "text-orange-600" : "text-gray-600"
+          }
+          onClick={() => handleClick('upvoted')}
+        >
+          upvote
+        </button>
+        <p>{props.song.votes}</p>
+        <button
+          className={
+            userVote === "downvoted" ? "text-orange-600" : "text-gray-600"
+          }
+          onClick={() => handleClick('downvoted')}
+        >
+          downvote
+        </button>
+      </div>
+    </div>
+  );
 }
