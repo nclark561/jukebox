6677c24e768b9b8b76a5261c91db7a3198961737
kaleck hamm <kaleckh@gmail.com>
2023-11-06 16:46:48 -0700

    up and down vote working well

--- src/app/socket/songDisplay.tsx
+++ src/app/socket/songDisplay.tsx
@@ -1,17 +1,17 @@
 "use client";
 import { useState, useEffect } from "react";
 import { Track } from "@spotify/web-api-ts-sdk";
-<<<<<<< HEAD
+
 import Image from "next/image";
 import styles from '../page.module.css'
-=======
+
 import { useSession } from "next-auth/react";
 
 interface Vote {
   voted: string;
   user: string;
 }
->>>>>>> 1db5abe712bd56557a620f57cc39ceb4803f0617
+// >>>>>>> 1db5abe712bd56557a620f57cc39ceb4803f0617
 
 interface QueueTrack extends Partial<Track> {
   votes: Vote[];
@@ -25,14 +25,11 @@ interface DispSongProps {
 
 export default function SongDisplay(props: DispSongProps) {
   const [userVote, setUserVote] = useState("neutral");
-<<<<<<< HEAD
-  
-=======
+
   const session = useSession()
-  const {socket, setQueue} = props
+  const { socket, setQueue } = props
 
   console.log(userVote)
->>>>>>> 1db5abe712bd56557a620f57cc39ceb4803f0617
 
   const handleClick = (vote: string) => {
     if (userVote === 'upvoted' && vote === 'upvoted') vote = 'neutral'
@@ -44,35 +41,17 @@ export default function SongDisplay(props: DispSongProps) {
   };
 
   return (
-<<<<<<< HEAD
+
     <div className={styles.flex}>
-      <p style={{textAlign:"center", width:"200px"}}>{props.song.name}</p>
-      <div className={styles.songContainer}>
-        <Image  onClick={() => {handleClick('upvoted') }} alt={'up arrow'} src={'/up.png'} width={30} height={30}></Image>
-        {/* <p>{props.song.votes}</p> */}
-        <Image onClick={() => handleClick('downvoted')} alt={'up arrow'} src={'/down.png'} width={30} height={30}></Image>
-=======
-    <div className="flex justify-between">
-      <p>{props.song.name}</p>
-      <div>
-        <button
-          className={
-            userVote === "upvoted" ? "text-orange-600" : "text-gray-600"
-          }
-          onClick={() => handleClick('upvoted')}
-        >
-          upvote
-        </button>
-        <button
-          className={
-            userVote === "downvoted" ? "text-orange-600" : "text-gray-600"
-          }
-          onClick={() => handleClick('downvoted')}
-        >
-          downvote
-        </button>
->>>>>>> 1db5abe712bd56557a620f57cc39ceb4803f0617
+      <p style={{ textAlign: "center", width: "200px" }}>{props.song.name}</p>
+
+      <Image onClick={() => { handleClick('upvoted') }} alt={'up arrow'} src={'/up.png'} width={30} height={30}></Image>
+      {/* <p>{props.song.votes}</p> */}
+      <Image onClick={() => handleClick('downvoted')} alt={'up arrow'} src={'/down.png'} width={30} height={30}></Image>
+      <div className="flex justify-between">
+        
+        
       </div>
     </div>
-  );
+  )
 }
