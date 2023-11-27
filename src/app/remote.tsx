2c9531b8a44647aa870cefa49ab033a5115d7225
kaleck hamm <kaleckh@gmail.com>
2023-11-27 16:02:54 -0700

    audio progress bar working

--- src/app/remote.tsx
+++ src/app/remote.tsx
@@ -2,6 +2,7 @@
 import { useEffect, useState } from "react";
 import styles from "./queue.module.css";
 import Image from "next/image";
+import { count } from "console";
 
 interface RemoteProps {
   session: any;
@@ -12,7 +13,10 @@ interface RemoteProps {
 export default function Remote({ session, socket, setQueue }: RemoteProps) {
   const [device, setDevice] = useState();
   const [play, setPlay] = useState(true);
+  const [queueT, setQueueT] = useState(false);
+  const [percent, setPercent] = useState<number>(0);
   const [current, setCurrent] = useState<any>();
+  const [counter, setCounter] = useState<number>(0);
   // useEffect(() => {
   //   const room = localStorage.getItem("room")
   //   if (room) {
@@ -22,17 +26,44 @@ export default function Remote({ session, socket, setQueue }: RemoteProps) {
   //   }
   // }, []);
 
+  useEffect(() => {
+    var data = localStorage.getItem("song")
+    if (data !== null) {
+      setCurrent(JSON.parse(data))
+    }
+  }, [])
+
+  function loader() {
+    let number = 0
+
+    setInterval(() => {
+      if (percent === 100) {
+        clearInterval()
+      }
+      number = number + 1
+      setCounter(number * 1000)
+      console.log('working')
+      // adder()        
+    }, 1000)
+    // return clearInterval()
+  }
+  useEffect(() => {
+    let info = counter / current?.duration_ms
+    console.log(info, "this is the decimal percent")
+    console.log(info * 100, "this is the Full Percent")
+    setPercent(info * 100)
+  }, [counter])
   return (
     <div className={styles.playContainer}>
-      <div style={{ display: "flex", width: "18%", justifyContent: "space-evenly" }}>
+      <div style={{ display: "flex", width: "25%", justifyContent: "space-evenly" }}>
         {current && (
           <>
-            <div>
+            <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
               <Image src={`${current?.album?.images[0]?.url}`} alt="current-song" width={50} height={50}></Image>
             </div>
-            <div>
-              <div className={styles.songTitleSmall}>{current?.name}</div>
-              <div className={styles.miniTitle}>{current?.album.name}</div>
+            <div className={styles.center}>
+              <div style={{ width: "250px" }} className={styles.songTitleSmall}>{current?.name}</div>
+              <div className={styles.miniTitle}>{current?.album?.name}</div>
             </div>
           </>
         )}
@@ -43,11 +74,10 @@ export default function Remote({ session, socket, setQueue }: RemoteProps) {
           {play ? (
             <Image
               onClick={() => {
-                console.log('successs mother fucker')
                 const room = localStorage.getItem("room");
                 socket.emit("play-queue", room, (response: any) => {
-                  console.log(response.currPlaying.album.images[0], 'this is info');
                   setCurrent(response.currPlaying)
+                  localStorage.setItem("song", `${JSON.stringify(response.currPlaying)}`)
                   if ("queue" in response) setQueue(response.queue);
                   if ("currPlaying" in response) console.log(response.currPlaying);
                 });
@@ -80,8 +110,9 @@ export default function Remote({ session, socket, setQueue }: RemoteProps) {
             width={50}
           />
         </div>
-        <div style={{marginTop:"10px"}} className="w-80 bg-gray-200 rounded-full h-1 mb-4 dark:bg-gray-700">
-          <div className="bg-blue-600 h-1 rounded-full dark:bg-blue-500" style={{ width: `${10}%` }}></div>
+        <button onClick={() => { loader() }} style={{ padding: "5px", backgroundColor: "white" }}>kale</button>
+        <div style={{ marginTop: "10px" }} className="w-80 bg-gray-200 rounded-full h-1 mb-4 dark:bg-gray-700">
+          <div className="bg-blue-600 h-1 rounded-full dark:bg-blue-500" style={{ width: `${percent}%` }}></div>
         </div>
       </div>
       <div style={{ width: "200px" }}></div>
